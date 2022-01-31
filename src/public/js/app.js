0479cb4781b625bc0a87b628ab1d47bbfb7da1a5
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-02-01 01:40:27 +0900

    Feat: videoSender

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -85,6 +85,13 @@ const handleCameraClick = () => {
 
 const handleCameraChange = async () => {
   await getMedia(cameraSelect.value);
+  if (myPeerConnection) {
+    const videoTrack = myStream.getVideoTracks()[0];
+    const videoSender = myPeerConnection
+      .getSenders()
+      .find((sender) => sender.track.kind === 'video');
+    videoSender.replaceTrack(videoTrack);
+  }
 };
 
 muteBtn.addEventListener('click', handleMuteClick);
